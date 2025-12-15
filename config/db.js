622df0839daed0e95f1e1410f24f8446dcab4130
Sachin Rathod <scrathod921@gmail.com>
2025-12-15 11:35:25 +0530

    DB cred move to env file

--- config/db.js
+++ config/db.js
@@ -1,7 +1,13 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://root:12345@cluster0.0dixxqr.mongodb.net/fod-del').then(()=>{
-        console.log("DB Connected");
-    })
-}
+  try {
+    await mongoose.connect(process.env.DB_URL);
+    console.log("DB Connected");
+  } catch (error) {
+    console.error("DB connection failed:", error.message);
+  }
+};
